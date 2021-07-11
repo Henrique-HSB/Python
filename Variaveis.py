1daf167a9d5a14da4fa5d85ede02df29f1a38d17
henriquehsb09 <80795657+henriquehsb09@users.noreply.github.com>
2021-07-11 10:25:04 -0300

    Create Variaveis.py
    estrutura de variáveis no Python

---
+++ Variaveis.py
@@ -0,0 +1,18 @@
+'''
+Variáveis são pedacinhos de memória onde armazenamos valores. 
+Sempre que referenciamos o nome de uma variável, o valor é acessado. 
+Definimos uma variável dando um nome a ela e usando o sinal de igual (=) para atribuir um valor.
+'''
+
+x = -10 # uma variável do tipo inteiro (int)
+y = 3.14 # uma variável do tipo real (float)
+escola = "Let's Code" # uma variável literal (string)
+primeiraAula = True # uma variável lógica (booleana)
+
+# Linhas iniciadas com # são comentários.
+# Comentários são ignorados pelo Python e servem para explicar o código.
+'''
+O símbolo # é um comentário de apenas 1 linha.
+Usando 3 aspas simples consecutivas é possível abrir um bloco de comentário
+de múltiplas linhas. O bloco se encerra com outras 3 aspas simples.
+'''
