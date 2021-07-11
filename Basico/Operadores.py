16f4f1620cb34e3181cb5df5908d361821bf8002
henriquehsb09 <80795657+henriquehsb09@users.noreply.github.com>
2021-07-11 10:28:35 -0300

    Create Operadores.py
    Operações aritméticas.

---
+++ Basico/Operadores.py
@@ -0,0 +1,19 @@
+# Podemos fazer operações aritméticas simples
+a = 2 + 3  # Soma
+b = 2 - 3  # Subtração
+c = 2 * 3  # Multiplicação
+d = 2 / 3  # Divisão
+e = 2 // 3 # Divisão inteira
+f = 2 ** 3 # Potência
+g = 2 % 3  # Resto de divisão
+
+print (a, b, c, d, e, f, g)
+
+# Podemos fazer operações dentro do print
+
+print (a+1, b+1)
+
+#Podemos fazer operações com variáveis não inteiras
+nome = input('Digite seu primeiro nome:')
+nome = nome + ' Schmitt'
+print(nome)
