98566b4251f8490dd6e1fb68a86565492b1f419a
henriquehsb09 <80795657+henriquehsb09@users.noreply.github.com>
2021-07-11 10:35:59 -0300

    Update Operadores.py
    Operadores aritméticos;
    Operadores relacionais;
    Operadores lógicos.

--- Basico/Operadores.py
+++ Basico/Operadores.py
@@ -17,3 +17,30 @@ print (a+1, b+1)
 nome = input('Digite seu primeiro nome:')
 nome = nome + ' Schmitt'
 print(nome)
+
+#Operadores relacionais
+comparacao1 = 5 > 3
+print(comparacao1)
+comparacao2 = 5 < 3
+print(comparacao2)
+
+'''
+O Python possui 6 operadores relacionais:
+Maior que: >
+Maior ou igual: >=
+Menor que: <
+Menor ou igual: <=
+Igual: ==
+Diferente: !=
+'''
+
+#Operadores lógicos
+#and: verdadeiro se condição 1 for verdadeira e condição 2 for verdadeira
+comparacao1 = 5 > 3 and 6 > 3
+comparacao2 = 5 < 3 and 6 > 3
+
+#or: verdadeiro se condição 1 for verdadeira ou condição 2 for verdadeira
+comparacao1 = 5 > 3 or 6 > 3
+comparacao2 = 5 < 3 or 6 > 3
+
+#Também é possível negar uma expressão lógica usando o not. Em outras palavras, se comparacao1 = 5 > 3 é verdadeira, comparacao1 = not(5 > 3) será falsa, e vice-versa.
