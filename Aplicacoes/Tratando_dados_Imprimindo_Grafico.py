3a993a6ebb9d271052e7b9d0021854b96cae4800
henriquehsb09 <80795657+henriquehsb09@users.noreply.github.com>
2021-07-18 19:45:41 -0300

    Tratando dados e Imprime o Gráfico
    #Chart

--- Aplicacoes/Tratando_dados_Imprimindo_Grafico.py
+++ Aplicacoes/Tratando_dados_Imprimindo_Grafico.py
@@ -111,3 +111,82 @@ final_data
 
 '''
 
+def get_datasets(y, labels): #Função para definir os dados
+    if type(y[0]) == list:
+        datasets = []
+        for i in range(len(y)):
+            datasets.append({
+                'label': labels[i],
+                'data':y[i]  
+            })
+        return datasets
+    else:
+        return[
+            {
+                'label': labels[0],
+                'data': y
+            }
+        ]
+     
+def set_title(title=''): # Função para definir o titulo
+    if title != "":
+        display= 'true'
+    else:
+        display = 'false'
+    return{
+        'title': title,
+        'display': display
+    }
+
+def creat_chart(x, y, labels, kind='bar', title=''):# Função para definir o grafico
+    
+    datasets = get_datasets(y, labels)
+    options = set_title(title)
+    
+    chart = { # dicionario representativo dos dados 
+        'type': kind,
+        'data':{
+            'labels': x,
+            'datasets': datasets
+        },
+        'options': options # identificação de eixos
+    }
+
+    return chart
+   
+def get_api_chart(chart):# Função que realiza a requisição da API utilizando o dicionario da finção anterior
+    url_base = 'https://quickchart.io/chart' # faz a requisição dos dados
+    resp = r.get(f'{url_base}?c={str(chart)}') # Retorna o arquivo da imagem
+    return resp.content #armazena o valor em binario
+
+def save_image(path, content): # Comando para salvar a imagem carregada
+    with open(path, 'wb') as image:
+        image.write(content)
+
+from PIL import Image# importa da biblioteca PIL  a função Imagem
+from IPython.display import display# importa da biblioteca IPython.display a função display
+
+def display_image(path):
+    img_pil = Image.open(path)
+    display(img_pil)
+
+y_data_1 = []
+for obs in final_data[1::10]: #pula de 10 em 10 dias a coleta de informações para plotar no grafico
+    y_data_1.append(obs[CONFIRMADOS]) # popula a variavel y_data_1 com os dados da da coluna CONFIRMADOS
+
+y_data_2 = []
+for obs in final_data[1::10]: #pula de 10 em 10 dias a coleta de informações para plotar no grafico
+    y_data_2.append(obs[RECUPERADOS]) # popula a variavel y_data_2 com os dados da da coluna RECUPERADOS
+
+labels = ['Confirmados', 'Recuperados']
+
+x = []
+for obs in final_data[1::10]: #pula de 10 em 10 dias a coleta de informações para plotar no grafico
+    x.append(obs[DATA].strftime('%d/%m/%Y'))# popula a variavel x com os dados da da coluna DATA
+    
+chart = creat_chart(x, [y_data_1, y_data_2], labels, title= 'Grafico confirmados vs recuperados')
+chart_content = get_api_chart(chart)
+save_image('meu-primeiro-grafico.png', chart_content)
+display_image('meu-primeiro-grafico.png') #Imprime o Grafico
+
+
